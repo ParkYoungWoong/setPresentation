06fff7b6a9fc91b3d2394c2df0b6474ba6ae8b0c
ParkYoungWoong <thesecon@gmail.com>
2017-08-11 17:30:31 +0900

    I've finished working on the 'flex' properties of the CSS3 presentation.
    I created a module function called 'REVEAL_CUSTOMIZER' in the 'reveal.custom.js'.

--- js/reveal.custom.js
+++ js/reveal.custom.js
@@ -1,5 +1,7 @@
+var REVEAL_CUSTOMIZER = {};
+
 // View Copyright.
-(function (window, document, $) {
+(function ($) {
   $(document).ready(function () {
     Reveal.addEventListener('first_page', function() {
       $.ajax({
@@ -11,24 +13,30 @@
       });
     });
   });
-}(window, document, jQuery));
+}(jQuery));
 
 
 // Slide change event.
-(function (window, document) {
+(function () {
   Reveal.addEventListener('slidechanged', function (event) {
-    var presentationTitle = event.currentSlide.dataset.presentTitle;
-    var presentationTitleElement = document.getElementById('presentation_title');
-
-    // if there is no '#presentation_title' element in the global slide area, exits a function.
-    if (!presentationTitleElement) return;
-
-    if (presentationTitle === undefined || presentationTitle === '') {
-      presentationTitleElement.classList.remove('active');
-      presentationTitleElement.innerHTML = '';
-    } else {
-      presentationTitleElement.classList.add('active');
-      presentationTitleElement.innerHTML = presentationTitle;
-    }
+    REVEAL_CUSTOMIZER.checkTitle();
   });
-}(window, document));
+}());
+
+
+// Check a title.
+REVEAL_CUSTOMIZER.checkTitle = function () {
+  var presentationTitle = event.currentSlide.dataset.presentTitle;
+  var presentationTitleElement = document.getElementById('presentation_title');
+
+  // if there is no '#presentation_title' element in the global slide area, exits a function.
+  if (!presentationTitleElement) return;
+
+  if (presentationTitle === undefined || presentationTitle === '') {
+    presentationTitleElement.classList.remove('active');
+    presentationTitleElement.innerHTML = '';
+  } else {
+    presentationTitleElement.classList.add('active');
+    presentationTitleElement.innerHTML = presentationTitle;
+  }
+};
